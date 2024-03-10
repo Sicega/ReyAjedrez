67249eeffbf61c6a2fc5fe6cbdf12f3bfc8aa1de
You-loop-great <silvia.cg12@gmail.com>
2024-03-10 14:18:52 +0100

    Creo enum Color

---
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Color.java
@@ -0,0 +1,20 @@
+package org.iesalandalus.programacion.reyajedrez.modelo;
+
+public enum Color {
+
+    BLANCO("Blanco"),
+    NEGRO("Negro");
+
+    private String cadenaAMostrar;
+
+    Color(String cadenaAMostrar){
+        this.cadenaAMostrar=cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return "Color{" +
+                "cadenaAMostrar='" + cadenaAMostrar + '\'' +
+                '}';
+    }
+}
