f64a55071a36c06f9d3a392ee6f79bc559f84786
You-loop-great <silvia.cg12@gmail.com>
2024-03-10 14:16:36 +0100

    Creo enum Direccion

---
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Direccion.java
@@ -0,0 +1,27 @@
+package org.iesalandalus.programacion.reyajedrez.modelo;
+
+public enum Direccion {
+
+    NORTE("Norte"),
+    NORESTE("Noreste"),
+    ESTE("Este"),
+    SURESTE("Sureste"),
+    SUR ("Sur"),
+    SUROESTE("Suroeste"),
+    OESTE("Oeste"),
+    NOROESTE("Noroeste"),
+    ENROQUE_CORTO("Enroque corto"),
+    ENROQUE_LARGO("Enroque largo");
+
+    private String cadenaAMostrar;
+    Direccion(String cadenaAMostrar){
+        this.cadenaAMostrar=cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return "Direccion{" +
+                "cadenaAMostrar='" + cadenaAMostrar + '\'' +
+                '}';
+    }
+}
