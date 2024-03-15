77bbf3504f0dd5df82b7d57fef32fe8a555f9d40
You-loop-great <silvia.cg12@gmail.com>
2024-03-15 18:09:51 +0100

    Creo atributo Rey en Mainapp

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,7 +1,13 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import org.iesalandalus.programacion.reyajedrez.modelo.Rey;
+
 public class MainApp {
+    private Rey rey;
+
     public static void main(String[] args) {
+
+
 
     }
 }
